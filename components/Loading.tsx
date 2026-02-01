e3072c3e2f7f9b396d8121392221110a49b5ede0
yamin <yamin148.infinity@gmail.com>
2026-02-01 17:12:22 +0700

    added loading

---
+++ components/Loading.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Lottie from 'lottie-react'
+import loadingAnimation from "../public/lottie/loading.json"
+
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-[#11001F] z-9999">
+      <Lottie
+        animationData={loadingAnimation}
+        loop
+        autoplay
+        style={{ width: 180, height: 180 }}
+      />
+    </div>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,26 @@
+'use client'
+
 import Layout from "@/components/Layout";
 import { SidebarToggleProvider } from "@/context/SidebarToggleContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import Loading from "@/components/Loading";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const init = async () => {
+      await new Promise((res) => setTimeout(res, 1200));
+      setLoading(false);
+    };
+
+    init();
+  }, []);
+
+  if (loading) return <Loading />
+
   return (
     <SidebarToggleProvider>
       <Layout>
